3318811e89587d5567d455ed3a1cad12b8208e5d
fatma20010 <fatmahammedi51@gmail.com>
2025-02-27 19:36:14 +0100

    update

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ from sentence_transformers import util
 from together import Together
 from fpdf import FPDF  # For generating PDFs
 import os
-
-
 nltk.download('punkt')
+nltk.download('punkt_tab')
 
 # Configuration
 CODES = {
