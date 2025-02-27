f8e81e25d4eb90ede6ee25fedd2668c8df97c25a
fatma20010 <fatmahammedi51@gmail.com>
2025-02-27 20:05:28 +0100

    update

--- app.py
+++ app.py
@@ -14,9 +14,15 @@ from fpdf import FPDF  # For generating PDFs
 import os
 
 # Download required NLTK data files
+import streamlit as st
+import nltk
+
+# Download required NLTK data files
+st.write("Downloading NLTK data files...")
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
+st.write("NLTK data files downloaded successfully!")
 # Configuration
 CODES = {
     "Tunisia": "TN.txt",
